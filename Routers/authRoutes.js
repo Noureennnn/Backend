3682909ced902639396d3cbe0c202bfb4efeb661
Noureen Khaled <noureenkhaled2004@gmail.com>
2025-09-04 11:14:45 +0300

    Fixed code

--- Routers/authRoutes.js
+++ Routers/authRoutes.js
@@ -3,6 +3,7 @@ const { loginUser, registerUser } = require("../Controllers/authController");
 
 const router = express.Router();
 router.post("/login", loginUser);
+router.post("/", loginUser);
 router.post("/register", registerUser);
 
 module.exports = router;
